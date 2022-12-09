fafb4837d0ae8fb1d04f86932daf909eba08b46b
haroldhalog <95028802+haroldhalog@users.noreply.github.com>
2022-12-09 21:44:47 +0800

    Update settings.py

--- pdda_django/settings.py
+++ pdda_django/settings.py
@@ -133,8 +133,6 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "staticfiles"),
-    
-STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 ]
 
 #MEDIA
