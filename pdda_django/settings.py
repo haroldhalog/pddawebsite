cdd5ac3f7c9b24c14941b6b90a64260950527dd9
haroldhalog <95028802+haroldhalog@users.noreply.github.com>
2022-12-09 22:15:43 +0800

    Update settings.py

--- pdda_django/settings.py
+++ pdda_django/settings.py
@@ -34,24 +34,6 @@ if DEBUG:
 
 ALLOWED_HOSTS = ['*']
 
-CORS_ALLOW_CREDENTIALS = True
-
-CORS_ORIGIN_ALLOW_ALL = True
-
-CORS_ALLOW_CREDENTIALS = True
-CSRF_TRUSTED_ORIGINS = ['https://front.bluemix.net/']
-
-CORS_REPLACE_HTTPS_REFERER = True
-
-CSRF_COOKIE_DOMAIN = 'bluemix.net'
-
-CORS_ORIGIN_WHITELIST = (
-    'https://front.bluemix.net/',
-    'front.bluemix.net',
-    'bluemix.net',
-)
-
-
 # Application definition
 
 INSTALLED_APPS = [
