8d20c24e8a050292c1a2ce7f16ffecfe41c51249
haroldhalog <95028802+haroldhalog@users.noreply.github.com>
2022-12-09 20:23:11 +0800

    Update settings.py

--- pdda_django/settings.py
+++ pdda_django/settings.py
@@ -122,16 +122,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT = '/var/www/staticfiles'
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+STATIC_URL = '/static/'
+STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 ]
 
 #MEDIA
 db_from_env = dj_database_url.config(conn_max_age=500)
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / "media/"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+urlpatterns += static(settings.STATIC_URL, documents_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, documents_root=settings.MEDIA_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
