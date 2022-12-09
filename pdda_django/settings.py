800a4131926aa8f198c7b7bbe591693cc63b9ddf
haroldhalog <95028802+haroldhalog@users.noreply.github.com>
2022-12-09 21:24:44 +0800

    Update settings.py

--- pdda_django/settings.py
+++ pdda_django/settings.py
@@ -126,9 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT = '/var/www/staticfiles'
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "staticfiles"),
 ]
 
 #MEDIA
